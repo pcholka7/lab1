146424896a07c1f2764d69baa1dadf325aef30fa
pcholka7 <161846958+pcholka7@users.noreply.github.com>
2024-03-15 09:30:30 +0700

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Enter the first number : ";
+	cin >> a;
+	cout << "Enter the second number : ";
+	cin >> b;
+	cout << "Enter the third number : ";
+	cin >> c;
+	int perimeter = a + b + c;
+	cout << "Perimeter: " << perimeter << endl;
+	cout << "Square:" << sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c)) << endl;
+	if (a == b or a == c or b == c) {
+		cout << "Triangle is isosceles.";
+	}
+	else {
+		cout << "Triangle is equilateral.";
+	}
+
+	
+
+
+}
