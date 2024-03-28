c49ad62e375543fa2f93eb9e19a2f88a80276be7
Kaiter94 <109782963+Kaiter94@users.noreply.github.com>
2024-03-28 23:07:27 +0700

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main() {
 	int a, b;
-	cout << "Enter the first number : ";
+	cout << "Enter the first number: ";
 	cin >> a;
-	cout << "Enter the second number : ";
+	cout << "Enter the second number: ";
 	cin >> b;
 	int perimeter = 2*(a + b);
 	cout << "Perimeter: " << perimeter << endl;
