d1735dbed9c9bfd16eca50b9035de22cc79a04e0
Kaiter94 <merkulovki94@gmail.com>
2024-03-29 10:25:30 +0700

    Update

--- triangle.cpp
+++ triangle.cpp
@@ -11,17 +11,20 @@ int main() {
 	cin >> b;
 	cout << "Enter the third number: ";
 	cin >> c;
-	int perimeter = a + b + c;
-	cout << "Perimeter: " << perimeter << endl;
-	cout << "Square:" << sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c)) << endl;
-	if (a == b or a == c or b == c) {
-		cout << "Triangle is isosceles.";
+	if (a + b < c or a + c < b or b + c < a or a < 0 or b < 0 or c < 0) {
+		cout << "Incorrect defines.";
 	}
 	else {
-		cout << "Triangle is equilateral.";
+		int perimeter = a + b + c;
+		cout << "Perimeter: " << perimeter << endl;
+		cout << "Square:" << sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c)) << endl;
+		if (a == b or a == c or b == c) {
+			cout << "Triangle is isosceles.";
+		}
+		else {
+			cout << "Triangle is equilateral.";
+		}
 	}
-
-	
 
 
 }
