5b613417e2bf1dbd863569b36fae49913b2c1faf
pcholka7 <alisa.krasina2005@mail.ru>
2024-03-29 10:25:12 +0300

    update

--- rectangle.cpp
+++ rectangle.cpp
@@ -9,9 +9,14 @@ int main() {
 	cin >> a;
 	cout << "Enter the second number : ";
 	cin >> b;
-	int perimeter = 2*(a + b);
-	cout << "Perimeter: " << perimeter << endl;
-	cout << "Square:" << (a*b) << endl;
-	cout << "Diagonal length : "<< sqrt(a*a+b*b);
-	return 0;
+	if (a < 0 or b < 0) {
+		cout << "Ne correctnoe znachenie" << endl;
+	}
+	else {
+		int perimeter = 2 * (a + b);
+		cout << "Perimeter: " << perimeter << endl;
+		cout << "Square:" << (a * b) << endl;
+		cout << "Diagonal length : " << sqrt(a * a + b * b);
+		return 0;
+	}
 }
